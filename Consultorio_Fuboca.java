0860d604db2869fb0c501e4234e008bd66bb91d5
Bitgab <134526370+Bitgab@users.noreply.github.com>
2025-05-14 22:31:38 -0300

    Add files via upload

---
+++ Consultorio_Fuboca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.consultorio_fuboca;
+
+/**
+ *
+ * @author facil
+ */
+public class Consultorio_Fuboca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
